ccd5875f978de256cdb9802eef37c219ab5fcc94
TurtleWolfe <JonPohlner@gmail.com>
2020-12-14 14:16:31 -0500

    list of links & SVG

--- src/App.js
+++ src/App.js
@@ -11,6 +11,54 @@ function App() {
         </p>
         <a
           className="App-link"
+          href="https://github.com/TurtleWolfe/fccTempLate"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FreeCodeCamp.com React Template
+        </a>
+        <a
+          className="App-link"
+          href="https://www.twitch.tv/videos/836894977"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This Episode on Twitch
+        </a>
+        <a
+          className="App-link"
+          href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FreeCodeCamp.com Front End Projects
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Markdown Cheatsheet
+        </a>
+        <a
+          className="App-link"
+          href="https://css-tricks.com/essential-meta-tags-social-media/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          The Essential Meta Tags for Social Media
+        </a>
+        <a
+          className="App-link"
+          href="https://youtu.be/SKXkC4SqtRk?t=865"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Pages, Deploy & Domain: Traversy Media
+        </a>
+        <a
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
