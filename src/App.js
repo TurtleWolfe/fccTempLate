aae3ad919acf9ef19e08da6457aae9ab5a2f4c43
TurtleWolfe <JonPohlner@gmail.com>
2020-12-15 11:02:34 -0500

    font-awesome and added eslint to gitignore

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span className="App-hammer">
-          <img src={logo} className="App-logo" alt="logo" />
-        </span>
+        <a
+          className="App-link"
+          href="https://github.com/TurtleWolfe/fccTempLate"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="fccTempLate"
+        >
+          {/* fccTempLate */}
+          <span className="App-hammer">
+            <img src={logo} className="App-logo" alt="logo" />
+          </span>
+        </a>
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
@@ -16,14 +25,16 @@ function App() {
           href="https://github.com/TurtleWolfe/fccTempLate"
           target="_blank"
           rel="noopener noreferrer"
+          title="fccTempLate with Font-Awesome"
         >
-          FreeCodeCamp.com React Template
+          FreeCodeCamp.com React Template <i class="fab fa-font-awesome-alt"></i>
         </a>
         <a
           className="App-link"
           href="https://www.twitch.tv/videos/836894977"
           target="_blank"
           rel="noopener noreferrer"
+          title="This Episode on Twitch I build a template for my freeCodeCamp projects"
         >
           This Episode on Twitch
         </a>
@@ -33,6 +44,7 @@ function App() {
           href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-random-quote-machine"
           target="_blank"
           rel="noopener noreferrer"
+          title=" Random Quote Machine"
         >
           Random Quote Machine
         </a>
@@ -41,6 +53,7 @@ function App() {
           href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer"
           target="_blank"
           rel="noopener noreferrer"
+          title="Markdown Previewer"
         >
           Markdown Previewer
         </a>
@@ -49,6 +62,7 @@ function App() {
           href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-drum-machine"
           target="_blank"
           rel="noopener noreferrer"
+          title="Drum Machine"
         >
           Drum Machine
         </a>
@@ -57,6 +71,7 @@ function App() {
           href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-javascript-calculator"
           target="_blank"
           rel="noopener noreferrer"
+          title="JavaScript Calculator"
         >
           JavaScript Calculator
         </a>
@@ -65,6 +80,7 @@ function App() {
           href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-25--5-clockhttps://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-25--5-clock"
           target="_blank"
           rel="noopener noreferrer"
+          title="25 + 5 Clock"
         >
           25 + 5 Clock
         </a>
@@ -74,6 +90,7 @@ function App() {
           href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects"
           target="_blank"
           rel="noopener noreferrer"
+          title="FreeCodeCamp.com Front End Projects"
         >
           FreeCodeCamp.com Front End Projects
         </a>
@@ -83,6 +100,7 @@ function App() {
           href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links"
           target="_blank"
           rel="noopener noreferrer"
+          title="Markdown Cheatsheet"
         >
           Markdown Cheatsheet
         </a>
@@ -91,6 +109,7 @@ function App() {
           href="https://css-tricks.com/essential-meta-tags-social-media/"
           target="_blank"
           rel="noopener noreferrer"
+          title="The Essential Meta Tags for Social Media"
         >
           The Essential Meta Tags for Social Media
         </a>
@@ -99,6 +118,7 @@ function App() {
           href="https://youtu.be/SKXkC4SqtRk?t=865"
           target="_blank"
           rel="noopener noreferrer"
+          title="GitHub Pages, Deploy & Domain: Traversy Media"
         >
           GitHub Pages, Deploy & Domain: Traversy Media
         </a>
@@ -108,6 +128,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          title="Learn React"
         >
           Learn React
         </a>
