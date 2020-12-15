5d7202097d7d358bf24a3c08de0ac1eb4af86506
TurtleWolfe <JonPohlner@gmail.com>
2020-12-14 22:21:03 -0500

    scriptHammer logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <span className="App-hammer">
+          <img src={logo} className="App-logo" alt="logo" />
+        </span>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
