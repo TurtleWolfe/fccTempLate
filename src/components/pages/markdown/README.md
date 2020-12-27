1302c2ed7aa1a4b48b0158b6092ed2490f128703
TurtleWolfe <JonPohlner@gmail.com>
2020-12-27 00:11:34 -0500

    markdown passing 7 out of 8

---
+++ src/components/pages/markdown/README.md
@@ -0,0 +1,25 @@
+# A Header
+
+## A sub Header
+
+[I'm an inline-style link with title](https://www.google.com "Google's Homepage")
+
+`inline code`
+
+```bash
+a code block
+```
+
+1. a
+1. list
+1. item
+
+Inline-style:
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png 'Logo Title Text 1')
+
+> Blockquotes are very handy in email to emulate reply text.
+> This line is part of the same quote.
+
+Quote break. **bolded text**
+
+> This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. Oh, you can _put_ **Markdown** into a blockquote.
--- src/components/pages/markdown/markdown.js
+++ src/components/pages/markdown/markdown.js
@@ -1,32 +1,88 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import marked from 'marked';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
-export const MarkDown = () => {
-  return (
-    <Fragment>
-      <h4>
-        <a
-          className="App-link"
-          href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Markdown Previewer"
-        >
-          <i className="fab fa-markdown"></i> Markdown Previewer <i className="fab fa-markdown"></i>
-        </a>
-      </h4>
-      <h5>
-        <a
-          className="App-link"
-          href="https://www.twitch.tv/videos/836894977"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="This Episode on Twitch I build a template for my freeCodeCamp projects, with FontAwesome & Analytics"
-        >
-          <i className="fab fa-twitch"></i> These Episodes on Twitch <i className="fab fa-twitch"></i>
-        </a>
-      </h5>
-    </Fragment>
-  );
-};
+export class Markdown extends Component {
+  constructor(props) {
+    super(props);
+    // Initial State
+    this.state = {
+      markdown: `# H1 Header
+## h2 header
+[**\`inline code\`**](https://www.google.com "Google's Homepage")  
 
-export default MarkDown;
+\`\`\`bash
+code block
+\`\`\`  
+> Quote Block
+1. Inline-style:
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png 'Logo Title Text 1')  
+`,
+    };
+  }
+
+  static propTypes = {
+    markdown: PropTypes.string.isRequired,
+  };
+
+  updateMarkdown(markdown) {
+    this.setState({ markdown });
+  }
+
+  render() {
+    let { markdown } = this.state;
+    return (
+      <Fragment>
+        <h4>
+          <a
+            className="App-link"
+            href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Markdown Previewer"
+          >
+            <i className="fab fa-markdown"></i> Markdown Previewer <i className="fab fa-markdown"></i>
+          </a>
+        </h4>
+        <Form
+          style={{ width: '90%' }}>
+          <Form.Group controlId="editor" >
+            <Form.Control as="textarea"
+              className="bg-dark text-white"
+              rows={3}
+              value={markdown}
+              onChange={(e) => {
+                this.updateMarkdown(e.target.value);
+              }}
+            />
+            <Form.Text id="passwordHelpBlock" muted>
+              enter markdown in the text area above
+      </Form.Text>
+          </Form.Group>
+        </Form>
+
+        <Card id="preview"
+          className="bg-dark text-white"
+          style={{ width: '90%' }}
+          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        >
+        </Card>
+        <h5>
+          <a
+            className="App-link"
+            href="https://www.twitch.tv/videos/836894977"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="This Episode on Twitch I build a MarkDown Previewer"
+          >
+            <i className="fab fa-twitch"></i> These Episodes on Twitch <i className="fab fa-twitch"></i>
+          </a>
+        </h5>
+      </Fragment>
+    );
+  }
+}
+
+export default Markdown;
