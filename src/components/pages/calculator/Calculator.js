2aa6fba9dafa3494363976fa8e0417d04fb6acf9
TurtleWolfe <JonPohlner@gmail.com>
2021-01-02 23:57:50 -0500

    copy clear and seven

--- src/components/pages/calculator/Calculator.js
+++ src/components/pages/calculator/Calculator.js
@@ -12,16 +12,25 @@ export class Calculator extends Component {
     };
 
     // This binding is necessary to make `this` work in the callback
-    // this.setQuote = this.setQuote.bind(this);
-
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClear = this.handleClear.bind(this);
   }
+
   // static propTypes = {
   //   quote: PropTypes.string.isRequired,
   // };
 
+  // handleClick(id) { this.setState(state => ({ display: {id} })); }
+  handleClick() { this.setState(state => ({ display: 7 })); }
+  handleClear() { this.setState(state => ({ display: 0 })); }
+
   render() {
+
+    // let seven = 7;
+
     return (
-      <Container id="calculator">
+      <Container
+        id="calculator">
         <h4>
           <a
             className="App-link"
@@ -35,98 +44,157 @@ export class Calculator extends Component {
         </h4>
         {/* <br></br> */}
         <Row className="justify-content-center">
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={0} variant="warning" id="divide">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={"/"}
+            variant="warning"
+            id="divide">
             <h2>
               <i class="fas fa-divide"></i>
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={7} variant="success" id="seven">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1}
+            id="seven"
+            value="7"
+            onClick={this.handleClick}
+            // onClick={this.handleClick.bind(this, id)}
+            // onClick={this.handleClick.bind(this, this.props.value)}
+            variant="success"
+          >
             <h2>
               7
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={8} variant="success" id="eight">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={8}
+            variant="success"
+            id="eight">
             <h2>
               8
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={9} variant="success" id="nine">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={9}
+            variant="success"
+            id="nine">
             <h2>
               9
             </h2>
           </Col>
         </Row>
         <Row className="justify-content-center">
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={0} variant="warning" id="multiply">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={"*"}
+            variant="warning"
+            id="multiply">
             <h2>
               <i class="fas fa-times"></i>
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={4} variant="success" id="four">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={4}
+            variant="success"
+            id="four">
             <h2>
               4
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={5} variant="success" id="five">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={5}
+            variant="success"
+            id="five">
             <h2>
               5
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={6} variant="success" id="six">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={6}
+            variant="success"
+            id="six">
             <h2>
               6
             </h2>
           </Col>
         </Row>
         <Row className="justify-content-center">
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={0} variant="warning" id="subtract">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={"-"}
+            variant="warning"
+            id="subtract">
             <h2>
               <i class="fas fa-minus"></i>
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={1} variant="success" id="one">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={1}
+            variant="success"
+            id="one">
             <h2>
               1
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={2} variant="success" id="two">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={2}
+            variant="success"
+            id="two">
             <h2>
               2
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={3} variant="success" id="three">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={3}
+            variant="success"
+            id="three">
             <h2>
               3
             </h2>
           </Col>
         </Row>
         <Row className="justify-content-center">
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={0} variant="warning" id="add">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={"+"}
+            variant="warning"
+            id="add">
             <h2>
               <i class="fas fa-plus"></i>
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={0} variant="dark" id="decimal">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={"."}
+            variant="dark"
+            id="decimal">
             <h1 className="decim">
               .
             </h1>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={0} variant="success" id="zero">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={0}
+            variant="success"
+            id="zero">
             <h2>
               0
             </h2>
           </Col>
-          <Col as={Button} className="key-pad" xs={2} sm={2} md={1} lg={1} value={0} variant="warning" id="equals">
+          <Col as={Button}
+            className="key-pad" xs={2} sm={2} md={1} lg={1} value={"="}
+            variant="warning"
+            id="equals">
             <h2>
               <i class="fas fa-equals"></i>
             </h2>
           </Col>
         </Row>
         <Row className="justify-content-center">
-          <Col as={"h3"} className="key-pad" xs={4} sm={2} md={2} lg={2} id="display">
+          <Col as={"h3"}
+            className="key-pad" xs={4} sm={2} md={2} lg={2}
+            id="display">
             {this.state.display}
           </Col>
-          <Col as={Button} className="key-pad" xs={4} sm={3} md={2} lg={2} value={0} variant="info" id="clear">
+          <Col as={Button}
+            className="key-pad" xs={4} sm={3} md={2} lg={2} value={"c"}
+            onClick={this.handleClear}
+            variant="info"
+            id="clear">
             <h2>
               clear
             </h2>
